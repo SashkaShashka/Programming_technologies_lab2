f1c2a8b8a024544831da14cb116f75586afd3e26
SashkaShashka <62326372+SashkaShashka@users.noreply.github.com>
2021-11-12 14:11:57 +0400

    Исправили ошибки

--- Client.py
+++ Client.py
@@ -1,4 +1,5 @@
 from socket import *
+import sys
 import json
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -7,27 +8,36 @@ clientSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 hostIp = "127.0.0.1"
 portNumber = 9090
 
-clientSocket.connect((hostIp, portNumber))
-print('Ожидание другого игрока')
+try:
+    clientSocket.connect((hostIp, portNumber))
+except Exception:
+    print('Сервер не активен, попробуйте подключиться позже')
+    clientSocket.close()
+    sys.exit()
+	
 
 try:
-    serverMessage = json.loads(clientSocket.recv(1024).decode("utf-8"))  # введите ход
+    serverMessage = json.loads(clientSocket.recv(1024).decode("utf-8")) 
     print(serverMessage)
-
-    serverMessage = ''
-    while serverMessage != 'Ваш ход принят. Ждите пока соперник ответит':
-        clientMessage = input()
-        clientSocket.send(json.dumps(clientMessage).encode("utf-8"))
-
-        serverMessage = json.loads(clientSocket.recv(1024).decode("utf-8"))  # ожидайте или ход не распознан
+    if (serverMessage=='Ожидание другого игрока'):
+        serverMessage = json.loads(clientSocket.recv(1024).decode("utf-8"))  # введите ход
         print(serverMessage)
 
-    serverMessage = json.loads(clientSocket.recv(1024).decode("utf-8"))  # ход соперника
-    print(serverMessage)
+        serverMessage = ''
+        while serverMessage != 'Ваш ход принят. Ждите пока соперник ответит':
+            clientMessage = input()
+            clientSocket.send(json.dumps(clientMessage).encode("utf-8"))
 
-    serverMessage = json.loads(clientSocket.recv(1024).decode("utf-8"))  # исход игры
-    print(serverMessage)
+            serverMessage = json.loads(clientSocket.recv(1024).decode("utf-8"))  # ожидайте или ход не распознан
+            print(serverMessage)
+
+        serverMessage = json.loads(clientSocket.recv(1024).decode("utf-8"))  # ход соперника
+        print(serverMessage)
+
+        serverMessage = json.loads(clientSocket.recv(1024).decode("utf-8"))  # исход игры
+        print(serverMessage)
 
     clientSocket.close()
 except Exception as e:
     print('Возникла ошибка, отключение игры')
+    print(e)
